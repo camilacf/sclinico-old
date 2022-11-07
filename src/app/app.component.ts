d4ffba0bf69844cdac53defb8157ec0259352f9e
camila <camilacf2112@gmail.com>
2022-11-07 17:42:10 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { auth$ } from "@spms/utils";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sclinico-old';
+  isAuthenticated: boolean = false;
+  username: string = '';
+
+  constructor(private router: Router){
+  }
+
+  ngOnInit(){
+    auth$.subscribe((s:any) => {
+      this.isAuthenticated = s.authenticated;
+      this.username = s.username;
+    })
+  }
+
+  gotoApp(){
+    this.router.navigate(['/auth']);
+  }
 }
